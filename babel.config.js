0065bdeef1211f937881c9c7711ccead054b10c6
laurie <laurie@brandwatch.com>
2020-01-26 17:38:42 +0000

    fix build

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,20 @@
 module.exports = {
   plugins: ["@babel/plugin-proposal-class-properties"],
-  presets: ["@babel/preset-react", "@babel/preset-env"]
+  presets: [
+    "@babel/preset-react",
+    [
+      "@babel/preset-env",
+      {
+        modules: false,
+        targets: {
+          browsers: [
+            "last 2 Firefox versions",
+            "last 2 Chrome versions",
+            "last 2 Edge versions",
+            "last 2 Safari versions"
+          ]
+        }
+      }
+    ]
+  ]
 };
